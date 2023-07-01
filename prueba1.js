cc1f47db493f5906df6a2ea4ceda158ba8d0064d
LNFernandez <116033054+lezenf@users.noreply.github.com>
2023-07-01 20:23:02 -0300

    Add files via upload

---
+++ prueba1.js
@@ -0,0 +1,40 @@
+let nombre = "jhon"
+let contraseña = 12345
+
+
+ 
+do  { 
+nombre = (prompt ("ingrese su nombre"));
+if (nombre != "jhon")
+alert ("no tienes acceso")
+else (alert ("bienvenido"))} 
+while (nombre != "jhon"); 
+
+ 
+do  { 
+contraseña = (prompt ("ingrese su contraseña"));
+if (contraseña != 12345)
+alert ("no tienes acceso")
+else (alert ("acceso correcto"))} 
+while (contraseña != 12345); 
+
+`------------------------------------------------------------------------------`
+
+
+
+
+
+
+
+
+	
+	
+
+
+
+
+ 
+
+
+
+
